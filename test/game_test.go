722f171d7386564c1fafbd2e6c29f2f35d48ade0
nharu-0630 <8305330+nharu-0630@users.noreply.github.com>
2025-03-10 09:33:29 +0900

    [add] 手動テストを追加

--- test/game_test.go
+++ test/game_test.go
@@ -32,7 +32,7 @@ func TestGame(t *testing.T) {
 	t.Logf("Connecting to %s", u.String())
 
 	names := make([]string, config.Game.AgentCount)
-	for i := 0; i < config.Game.AgentCount; i++ {
+	for i := range config.Game.AgentCount {
 		const letterBytes = "abcdefghijklmnopqrstuvwxyz"
 		b := make([]byte, 8)
 		for i := range b {
@@ -42,7 +42,7 @@ func TestGame(t *testing.T) {
 	}
 
 	clients := make([]*DummyClient, config.Game.AgentCount)
-	for i := 0; i < config.Game.AgentCount; i++ {
+	for i := range config.Game.AgentCount {
 		client, err := NewDummyClient(u, names[i], t)
 		if err != nil {
 			t.Fatalf("Failed to create WebSocket client: %v", err)
@@ -58,6 +58,54 @@ func TestGame(t *testing.T) {
 		case <-time.After(30 * time.Second):
 			t.Fatalf("Timeout")
 		}
+	}
+
+	time.Sleep(5 * time.Second)
+	t.Log("Test completed successfully")
+}
+
+
+func TestManualGame(t *testing.T) {
+	config, err := model.LoadFromPath("../config/debug.yml")
+	if err != nil {
+		t.Fatalf("Failed to load config: %v", err)
+	}
+	if _, exists := os.LookupEnv("GITHUB_ACTIONS"); exists {
+		config.Server.WebSocket.Host = WebSocketExternalHost
+		return
+	}
+	go func() {
+		server := core.NewServer(*config)
+		server.Run()
+	}()
+	time.Sleep(5 * time.Second)
+
+	u := url.URL{Scheme: "ws", Host: config.Server.WebSocket.Host + ":" + strconv.Itoa(config.Server.WebSocket.Port), Path: "/ws"}
+	t.Logf("Connecting to %s", u.String())
+
+	names := make([]string, config.Game.AgentCount-1)
+	for i := range config.Game.AgentCount - 1 {
+		const letterBytes = "abcdefghijklmnopqrstuvwxyz"
+		b := make([]byte, 8)
+		for i := range b {
+			b[i] = letterBytes[rand.Intn(len(letterBytes))]
+		}
+		names[i] = string(b)
+	}
+
+	clients := make([]*DummyClient, config.Game.AgentCount-1)
+	for i := range config.Game.AgentCount - 1 {
+		client, err := NewDummyClient(u, names[i], t)
+		if err != nil {
+			t.Fatalf("Failed to create WebSocket client: %v", err)
+		}
+		clients[i] = client
+		defer clients[i].Close()
+	}
+
+	for _, client := range clients {
+		<-client.done
+		t.Log("Connection closed")
 	}
 
 	time.Sleep(5 * time.Second)
