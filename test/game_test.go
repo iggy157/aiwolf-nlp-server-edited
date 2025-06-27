439b70e098ac83be98a0c6242ca61014c41391b3
nharu-0630 <root@nharu.dev>
2025-06-27 09:58:01 +0900

    [update] テストサーバの起動を最適化

--- test/game_test.go
+++ test/game_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"math/rand"
+	"net"
 	"net/url"
 	"os"
 	"strconv"
@@ -9,23 +11,17 @@ import (
 
 	"github.com/aiwolfdial/aiwolf-nlp-server/core"
 	"github.com/aiwolfdial/aiwolf-nlp-server/model"
-	"github.com/joho/godotenv"
 )
 
 const WebSocketExternalHost = "0.0.0.0"
+const TestClientName = "aiwolf-nlp-viewer"
 
-func TestGame(t *testing.T) {
-	if _, exists := os.LookupEnv("GITHUB_ACTIONS"); !exists {
-		godotenv.Load("../config/.env")
-	}
-
-	config, err := model.LoadFromPath("../config/debug.yml")
-	if err != nil {
-		t.Fatalf("Failed to load config: %v", err)
-	}
+func launchAsyncServer(config *model.Config) url.URL {
 	if _, exists := os.LookupEnv("GITHUB_ACTIONS"); exists {
 		config.Server.WebSocket.Host = WebSocketExternalHost
 	}
+	port := getAvailableTcpPort()
+	config.Server.WebSocket.Port = port
 	go func() {
 		server, err := core.NewServer(*config)
 		if err != nil {
@@ -34,18 +30,34 @@ func TestGame(t *testing.T) {
 		server.Run()
 	}()
 	time.Sleep(5 * time.Second)
+	return url.URL{Scheme: "ws", Host: config.Server.WebSocket.Host + ":" + strconv.Itoa(config.Server.WebSocket.Port), Path: "/ws"}
+}
 
-	u := url.URL{Scheme: "ws", Host: config.Server.WebSocket.Host + ":" + strconv.Itoa(config.Server.WebSocket.Port), Path: "/ws"}
-	t.Logf("Connecting to %s", u.String())
-
-	names := make([]string, config.Game.AgentCount)
-	for i := range config.Game.AgentCount {
-		names[i] = "aiwolf-nlp-viewer"
+func getAvailableTcpPort() int {
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	port := rand.Intn(65535-49152+1) + 49152
+	for {
+		listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+		if err == nil {
+			listener.Close()
+			break
+		}
+		port = rand.Intn(65535-49152+1) + 49152
 	}
+	return port
+}
+
+func TestGame(t *testing.T) {
+	config, err := model.LoadFromPath("../config/debug.yml")
+	if err != nil {
+		t.Fatalf("Failed to load config: %v", err)
+	}
+	u := launchAsyncServer(config)
+	t.Logf("Connecting to %s", u.String())
 
 	clients := make([]*TestClient, config.Game.AgentCount)
 	for i := range config.Game.AgentCount {
-		client, err := NewRandomTestClient(t, u, names[i])
+		client, err := NewRandomTestClient(t, u, TestClientName)
 		if err != nil {
 			t.Fatalf("Failed to create WebSocket client: %v", err)
 		}
@@ -60,55 +72,6 @@ func TestGame(t *testing.T) {
 		case <-time.After(5 * time.Minute):
 			t.Fatalf("Timeout")
 		}
-	}
-
-	time.Sleep(5 * time.Second)
-	t.Log("Test completed successfully")
-}
-
-func TestManualGame(t *testing.T) {
-	if _, exists := os.LookupEnv("GITHUB_ACTIONS"); !exists {
-		godotenv.Load("../config/.env")
-	}
-
-	config, err := model.LoadFromPath("../config/debug.yml")
-	if err != nil {
-		t.Fatalf("Failed to load config: %v", err)
-	}
-	if _, exists := os.LookupEnv("GITHUB_ACTIONS"); exists {
-		config.Server.WebSocket.Host = WebSocketExternalHost
-		return
-	}
-	go func() {
-		server, err := core.NewServer(*config)
-		if err != nil {
-			return
-		}
-		server.Run()
-	}()
-	time.Sleep(5 * time.Second)
-
-	u := url.URL{Scheme: "ws", Host: config.Server.WebSocket.Host + ":" + strconv.Itoa(config.Server.WebSocket.Port), Path: "/ws"}
-	t.Logf("Connecting to %s", u.String())
-
-	names := make([]string, config.Game.AgentCount-1)
-	for i := range config.Game.AgentCount - 1 {
-		names[i] = "aiwolf-nlp-viewer"
-	}
-
-	clients := make([]*TestClient, config.Game.AgentCount-1)
-	for i := range config.Game.AgentCount - 1 {
-		client, err := NewRandomTestClient(t, u, names[i])
-		if err != nil {
-			t.Fatalf("Failed to create WebSocket client: %v", err)
-		}
-		clients[i] = client
-		defer clients[i].Close()
-	}
-
-	for _, client := range clients {
-		<-client.done
-		t.Log("Connection closed")
 	}
 
 	time.Sleep(5 * time.Second)
--- test/test_client.go
+++ test/test_client.go
@@ -55,6 +55,21 @@ func NewRandomTestClient(t *testing.T, u url.URL, name string) (*TestClient, err
 	})
 }
 
+func HandleTarget(tc TestClient) (string, error) {
+	if statusMap, exists := tc.info["status_map"].(map[string]any); exists {
+		for k, v := range statusMap {
+			if k == tc.info["agent"].(string) {
+				continue
+			}
+			if v == model.S_ALIVE.String() {
+				return k, nil
+			}
+		}
+		return "", errors.New("target not found")
+	}
+	return "", errors.New("status_map not found")
+}
+
 func (tc *TestClient) listen(t *testing.T) {
 	defer close(tc.done)
 	for {
